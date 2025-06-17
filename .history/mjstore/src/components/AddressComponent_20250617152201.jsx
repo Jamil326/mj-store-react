e6a1a4a601b69b1052a6ffc4b468e828e78948ff
jamil <mafuza326khatun@gmail.com>
2025-06-17 15:51:30 +0530

    up

---
+++ .history/mjstore/src/components/AddressComponent_20250617152201.jsx
@@ -0,0 +1,138 @@
+import { useState ,useEffect } from 'react';
+import { Button } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+
+const AddressForm = () => {
+    const redir = useNavigate();
+    const [ isAvailable , setisAvailable ] = useState(false)
+    const [ address,setAddress ] = useState({
+        street:'',
+        landmark:'',
+        city:'',
+        pin:'',
+        state:''
+
+
+    })
+
+    const handleChange =(e)=>{
+        
+        const {name,value} = e.target;
+        setAddress((prev)=>{
+           return {...prev ,[name]:value}
+
+        })
+    }
+
+    const handleSubmit= async (e) => {
+        e.preventDefault();
+        
+        
+        try {
+            const token = localStorage.getItem('token')|| "";
+            const user =JSON.parse(localStorage.getItem('user-info')||'{}');
+             
+            const { _id } =localStorage.getItem('user-info')|| "";
+            
+            
+            const url = 'https://mj-store.onrender.com/api/v1/user/address'
+        
+            if(!token) {
+                throw new Error('token required');
+            }
+
+            const res = await fetch(url,{
+                method:'POST',
+                mode:'cors',
+                headers:{
+                    'content-type':'application/json',
+                    Authorization:`Bearer ${token}`
+                },
+                body:JSON.stringify(address)
+            })
+
+            const result = await res.json();
+            
+            if(!res.ok){
+                throw new Error(result.message);
+            }
+
+            if(res.status===200){
+                toast.success(result.message||'successfull added address');
+                const user = JSON.parse(localStorage.getItem('user-info')||'{}');
+                
+                
+                
+            }
+            
+        } catch (error) {
+          return  toast.error(error.message)
+        }
+        
+    }
+
+    
+    return (
+        <div className='mt-3 shadow-normal  '>
+         <form onSubmit={handleSubmit} className='form-control d-flex flex-column gap-3 p-5'>
+                 <h1 className='text-center'>Fill Up Address</h1>
+
+             <input 
+             type="text" 
+             name="street" 
+             value={address.street}
+             placeholder='Street'
+             onChange={handleChange}
+             className='rounded p-2 border-0 border-bottom'
+             />
+
+             <input  
+             type="text" 
+             name="landmark" 
+             value={address.landmark}
+             placeholder='Landmark'
+             onChange={handleChange}
+             className='rounded p-2 border-0 border-bottom'
+             />
+
+             <input  
+             type="text" 
+             name="city" 
+             value={address.city}
+             placeholder='City'
+             onChange={handleChange}
+             className='rounded p-2 border-0 border-bottom'
+
+             />
+
+
+             <input  
+             type="number" 
+             name="pin" 
+             value={address.pin}
+             placeholder='Pin'
+             onChange={handleChange}
+             className='rounded p-2 border-0 border-bottom'
+             
+             />
+
+
+             <input  
+             type="text" 
+             name="state" 
+             value={address.state}
+             placeholder='State'
+             onChange={handleChange}
+             className='rounded p-2 border-0 border-bottom'
+             
+             />
+             <Button type='submit'  variant ='success mt-3 mb-3'>Add</Button>
+         </form>
+        </div>
+    )
+} 
+
+
+
+export default AddressForm;
---
+++ .history/mjstore/src/pages/Login_20250617143451.jsx
@@ -0,0 +1,139 @@
+import { Container , Col , Row, Form , Button , Spinner } from 'react-bootstrap';
+import { useState , useContext ,useEffect} from 'react';
+import { IoLogIn  } from "react-icons/io5";
+import { toast } from 'react-toastify';
+import { UserContext }from '../context/userContext';
+import { useNavigate } from 'react-router-dom';
+
+
+
+
+const Login = () => {
+    const redir = useNavigate();
+    const { logged ,user , setUser ,setLogged } = useContext ( UserContext );
+    const [ mobile , setMobile ] = useState('');
+    const [ password , setPassword ] = useState('');
+ 
+    const handleMobile = (e) => {
+     setMobile(e.target.value);
+       
+    }
+
+    const handlePassword = (e) => {
+     setPassword(e.target.value);
+       
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            mobile,
+            password
+        }
+
+     if( !data.mobile || data.mobile.length !==10){
+         toast.warn('Mobile Number Should BE 10 Digit');
+        return setMobile('');
+     }
+
+      if( !data.password || data.password.length !==10){
+         toast.warn('Password Should BE Atleast 6 Digit');
+        return setPassword('');
+     }
+
+        try {
+
+            
+
+        const urlStr = 'https://mj-store.onrender.com/api/v1/user/login';
+
+        const result = await fetch(urlStr,{
+            method:'POST',
+            mode:'cors',
+            headers:{
+                Accept:'application/json',
+                'content-type':'application/json'
+            },
+
+            body:JSON.stringify(data)
+
+        });
+         const res = await result.json();
+        if(result.status===401){
+            toast.warn(res.message);
+        }
+
+        if(result.status===404) {
+            toast.error(res.message);
+        }
+
+        if(!result.ok){
+         
+         throw new Error(res.message);   
+        }
+
+        const token = res.data.token;        
+        localStorage.setItem('token',token);
+        localStorage.setItem('user-info',JSON.stringify(res.data.existed));
+        setLogged(true);
+        toast.success(res.message);
+        console.log(res);
+        redir('/dashboard');
+
+
+        
+
+        } catch (error) {
+            toast.error(error);
+        }finally{
+           
+        }
+    }
+    return (
+       <Container className='mt-3'>
+       <Row className='justify-content-center'>
+       <Col md={6} >
+       <h2 className='text-center mt-3'>Login</h2>
+       <Form className='mt-5 form-control shadow'>
+       <Form.Label>Mobile</Form.Label>
+        <Form.Group className='mb-3' controlId='formMobile'>
+        <Form.Control
+        type='number'
+        name='mobile'
+        placeholder='Please Enter Mobile Number' 
+        value={mobile}
+        onChange={handleMobile}
+        />
+        </Form.Group>
+
+
+         <Form.Label>Password</Form.Label>
+        <Form.Group controlId='formPassword'>
+        <Form.Control
+        type='password'
+        name='password'
+        placeholder='Please Enter Password' 
+        value={password}
+        onChange={handlePassword}
+        />
+        </Form.Group>
+        <div className='d-flex justify-content-end' ><Button variant='danger' className='mt-3'>ForgetPassword?</Button></div>
+
+        <div className='d-grid mt-5 mb-5'>
+            <Button onClick={handleSubmit} variant='dark'>
+            <span className='px-3'>Submit</span>
+</Button>
+
+        </div>
+       </Form>
+       </Col>
+       </Row>
+       </Container>
+           
+       
+    );
+}
+
+
+
+export default Login;
