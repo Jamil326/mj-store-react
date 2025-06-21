20a531e0de453671ece4f4a219eb7d7e60ab3969
jamil <mafuza326khatun@gmail.com>
2025-06-21 15:21:52 +0530

    icon updated

--- .history/mjstore/vite.config_20250621151826.js
+++ .history/mjstore/vite.config_20250621151826.js
@@ -22,7 +22,7 @@ export default defineConfig({
         start_url: '/',
         icons: [
           {
-            src: '/icon-192.png',
+            src: '/icon-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
