d6f703ce220be23ffc05a893ac95ce653b6141b4
jamil <mafuza326khatun@gmail.com>
2025-06-30 13:57:10 +0530

    footer update

--- .history/mjstore/src/components/Footer_20250630135413.jsx
+++ .history/mjstore/src/components/Footer_20250630135413.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
   };
 
   useEffect(() => {
-    
+    fetchCartCount();
   }, []);
 
   return (
--- mjstore/dev-dist/sw.js
+++ mjstore/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.bvl2ckcccp8"
+    "revision": "0.omp7gbs48qo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
